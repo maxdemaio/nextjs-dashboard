3ec2e3d16be157bc29cdffaaa96bf988f9bbbb01
maxdemaio <maxdman321@gmail.com>
2023-11-14 08:54:57 -0500

    add data fetching

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children} test</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
