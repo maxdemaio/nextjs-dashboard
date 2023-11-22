fae73f3a2640a23c12cc507d5ca28b236e68cd27
maxdemaio <maxdman321@gmail.com>
2023-11-22 08:30:13 -0500

    finish metadata and course

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -3,6 +3,13 @@ import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
 import { notFound } from 'next/navigation';
 
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Edit Invoices',
+};
+ 
+
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const [invoice, customers] = await Promise.all([
